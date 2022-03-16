e5181c7a0613a405bf03867e8b65de25dcb928b7
António Inácio <ainacio@MacBook-Pro.lan>
2022-03-16 19:25:13 +0000

    refactor: Bootstrap application

---
+++ cmd/serve.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Run server",
+	Long:  `Run hermit-shell in server mode.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
