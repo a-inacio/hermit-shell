dec470ead5af44b0ff50dc08e6346a5f55dd016a
António Inácio <ainacio@MacBook-Pro.lan>
2022-03-21 18:55:40 +0000

    reafactor(WIP): Sample code to stream from GRPC

--- pkg/event/grpc.go
+++ pkg/event/grpc.go
@@ -1,6 +1,10 @@
 package event
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	proto "github.com/a-inacio/hermit-shell/api/github.com/a-inacio/hermit-shell-grpc"
 	"github.com/a-inacio/hermit-shell/internal/server"
 
@@ -22,5 +26,26 @@ func RegisterServer() *Server {
 }
 
 func (s *Server) SubscribeEvent(request *proto.SubscribeEventRequest, server proto.HermitShellEvent_SubscribeEventServer) error {
+	log := s.l
+
+	log.Info("SubscribeEvent: ", request.EventName)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(count int64) {
+			defer wg.Done()
+
+			time.Sleep(time.Duration(count) * time.Second)
+			resp := proto.SubscribeEventReply{Payload: fmt.Sprintf("Request #%d For Id:%s", count, request.EventName)}
+			if err := server.Send(&resp); err != nil {
+				log.Errorf("send error %v", err)
+			}
+			log.Infof("finishing request number : %d", count)
+		}(int64(i))
+	}
+
+	wg.Wait()
+
 	return nil
 }
